0e2eb7c2f2038ddab96ad026ff001372e76af1c7
hellokj <neil1996xyz@gmail.com>
2018-12-18 20:53:47 +0800

    Initial commit

---
+++ src/main/kotlin/com/kj/Hello.kt
@@ -0,0 +1,14 @@
+package com.kj
+
+fun main(args: Array<String>) {
+//    println("Hello kolin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human(){
+    fun hello(){
+        println("Hello kolin")
+    }
+}
